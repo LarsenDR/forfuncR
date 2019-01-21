6bc3de028d1588fe37f76f7661ae76e45ac74564
David Larsen <larsendr@missouri.edu>
2019-01-21 09:57:16 -0600

    Update acornwt.R
    Fixed the "And" in code

--- R/acornwt.R
+++ R/acornwt.R
@@ -8,11 +8,11 @@ acornwt = function(dbh, species)
     acornwt = -1.9065 + 0.2973 * dbh
   }else if((species == "chestnut") && (dbh > 9.9) && (dbh < 36.1)){
     acornwt = 0.0008271 * dbh ^ 3 - 0.08157 * dbh ^ 2 + 2.692 * dbh - 18.85
-  }else if(species = "red" And dbh > 9.9 And dbh < 36.1) {
+  }else if(species = "red" && dbh > 9.9 && dbh < 36.1) {
     acornwt = 0.0004016 * dbh ^ 4 - 0.0349937 * dbh ^ 3 + 0.9864357 * dbh ^ 2 - 9.5233885 * dbh + 27.32720516
-  }else if (species = "scarlet" And dbh > 9.9 And dbh < 36.1) Then
+  }else if (species = "scarlet" && dbh > 9.9 && dbh < 36.1) Then
     acornwt = 0.0005975 * dbh ^ 4 - 0.05325 * dbh ^ 3 + 1.651 * dbh ^ 2 - 19.97 * dbh + 84.71
-  }else if(species = "white" And dbh > 9.9 And dbh < 36.1){}
+  }else if(species = "white" && dbh > 9.9 && dbh < 36.1){}
     acornwt = 0.0001987 * dbh ^ 4 - 0.02027 * dbh ^ 3 + 0.694 * dbh ^ 2 - 8.768 * dbh + 37.36
   }else{}
     acornwt = 0.0
